46eca6a0fef8317da89e1aedf046a222b6156925
Niko2802 <simakov_n_e@mail.ru>
2023-04-06 11:07:35 +0300

    1

--- web_test/hello/urls.py
+++ web_test/hello/urls.py
@@ -5,7 +5,12 @@ urlpatterns = [
     path("", views.home, {"name": "home"}),
     path("about", views.about),
     path("blog/<int:year>/", views.blogs),
-    path("test", views.test)
+    path("test", views.test),
+    path("kapuchino", views.recipe, {"name": "kapuchino"}),
+    path("latte", views.recipe, {"name": "latte"}),
+    path("amerikano", views.recipe, {"name": "amerikano"}),
+    path("latte with syrup", views.recipe, {"name": "latte with syrup"}),
+    path("coffee with cream", views.recipe, {"name": "coffee with cream"})
     ]
 
 
--- web_test/hello/views.py
+++ web_test/hello/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+drinks = {
+    "kapuchino": {"milk": 200, "coffee": 15},
+    "latte": {"milk": 250, "coffee": 7, "water": 50},
+    "amerikano": {"water": 150, "coffee": 5},
+    "latte with syrup": {"milk": 250, "coffee": 7, "water": 50, "syrup": 10},
+    "coffee with cream": {"water": 150, "coffee": 10, "cream": 20}
+}
+
+
 def home(request, name):
     return HttpResponse(f"Hello django {name}")
 
@@ -17,3 +27,12 @@ def blogs(request, year):
 
 def test(request):
     return HttpResponseRedirect("about")
+
+def recipe(request, name):
+    rec = drinks.get(name)
+    ing = rec.keys()
+    ing_str = ""
+    for i in ing:
+        ing_str += i + " " + str(rec.get(i)) + "<br>"
+    page = f"<h2>Рецепт кофе {name}</h2><br>{ing_str}"
+    return HttpResponse(page)
