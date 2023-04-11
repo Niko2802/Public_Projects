3e78682194f08c51dedb35909e06b3834321b6eb
Niko2802 <simakov_n_e@mail.ru>
2023-04-11 08:55:14 +0300

    1

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,9 @@
 import requests
 from pyquery import PyQuery as pq
 
+
 def get_post_data(headers):
-    r = requests.get("https://morpher.ru/Demo.aspx", headers = headers)
+    r = requests.get("https://morpher.ru/Demo.aspx", headers=headers)
     post_data = {}
     if r.status_code == 200:
         html_text = r.text
@@ -16,12 +17,15 @@ def get_post_data(headers):
 
 
 s = "красный"
-mask = {"Именительный", "Рoдительный2", "Дательный", "Винительный", "Творительный", "Предложный"}
-headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
+mask = {"Именительный", "Рoдительный2", "Дательный",
+        "Винительный", "Творительный", "Предложный"}
+headers = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
 post_data = get_post_data(headers)
 headers["origin"] = "https://morpher.ru"
 headers["refer"] = "https://morpher.ru/Demo.aspx"
-r = requests.post("https://morpher.ru/Demo.aspx", headers = headers, data=post_data)
+r = requests.post("https://morpher.ru/Demo.aspx",
+                  headers=headers, data=post_data)
 print(r.status_code)
 if r.status_code == 200:
     html_text = r.text
