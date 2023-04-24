3dfdd3edadebfb27e8610ce5e1c3d71270e4cab8
Niko2802 <simakov_n_e@mail.ru>
2023-04-24 18:54:09 +0300

    1

--- web_test/hello/urls.py
+++ web_test/hello/urls.py
@@ -2,15 +2,18 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path("", views.home, {"name": "home"}),
-    path("about", views.about),
-    path("blog/<int:year>/", views.blogs),
-    path("test", views.test),
-    path("kapuchino", views.recipe, {"name": "kapuchino"}),
-    path("latte", views.recipe, {"name": "latte"}),
-    path("amerikano", views.recipe, {"name": "amerikano"}),
-    path("latte with syrup", views.recipe, {"name": "latte with syrup"}),
-    path("coffee with cream", views.recipe, {"name": "coffee with cream"})
+    path("", views.menu),
+    path("menu", views.menu),
+    path("menu/<str:name>", views.buy),
+    path("vault", views.vault)
+    # path("about", views.about),
+    # path("blog/<int:year>/", views.blogs),
+    # path("test", views.test),
+    # path("kapuchino", views.recipe, {"name": "kapuchino"}),
+    # path("latte", views.recipe, {"name": "latte"}),
+    # path("amerikano", views.recipe, {"name": "amerikano"}),
+    # path("latte with syrup", views.recipe, {"name": "latte with syrup"}),
+    # path("coffee with cream", views.recipe, {"name": "coffee with cream"})
     ]
 
 
--- web_test/hello/views.py
+++ web_test/hello/views.py
@@ -12,27 +12,56 @@ drinks = {
 }
 
 
-def home(request, name):
-    return HttpResponse(f"Hello django {name}")
+# def home(request, name):
+#     return HttpResponse(f"Hello django {name}")
 
-def about(request):
-    print(request.body)
-    print(request.method)
-    print(request.GET)
-    print(request.COOKIES)
-    return HttpResponse("Info about me")
+# def about(request):
+#     print(request.body)
+#     print(request.method)
+#     print(request.GET)
+#     print(request.COOKIES)
+#     return HttpResponse("Info about me")
 
-def blogs(request, year):
-    return HttpResponse(f"<h2>blog {year}</h2>")
+# def blogs(request, year):
+#     return HttpResponse(f"<h2>blog {year}</h2>")
 
-def test(request):
-    return HttpResponseRedirect("about")
+# def test(request):
+#     return HttpResponseRedirect("about")
 
-def recipe(request, name):
-    rec = drinks.get(name)
-    ing = rec.keys()
-    ing_str = ""
-    for i in ing:
-        ing_str += i + " " + str(rec.get(i)) + "<br>"
-    page = f"<h2>Рецепт кофе {name}</h2><br>{ing_str}"
-    return HttpResponse(page)
+# def recipe(request, name):
+#     rec = drinks.get(name)
+#     ing = rec.keys()
+#     ing_str = ""
+#     for i in ing:
+#         ing_str += i + " " + str(rec.get(i)) + "<br>"
+#     page = f"<h2>Рецепт кофе {name}</h2><br>{ing_str}"
+#     return HttpResponse(page)
+
+def menu(request):
+    context = {
+        "drinks": {
+    "kapuchino": "Капучино",
+    "latte": "Латте",
+    "amerikano": "Американо",
+    "latte with syrup": "Латте с сиропом",
+    "coffee with cream": "Кофе со сливками",
+    "raf": "Раф"}
+    }
+    return render(request, "menu.html", context=context)
+
+def vault(request):
+    pass
+
+def buy(request, name):
+    context = {}
+    return render(request, "ingridients.html", context=context)
+
+# Все элементы span нужно сделать жирными
+# Фон страницы сделать кофейного цвета
+# ! Сделать фон на всю страницу
+# Подключить код кофемашины к проекту Django
+# Сделать страницу Vault
+# Реализовать Post запрос на кнопку купить
+# 
+# 
+# 
