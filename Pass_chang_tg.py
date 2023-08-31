0d6f0c8749aa268324eeed97bda5322f48db9619
Niko2802 <simakov_n_e@mail.ru>
2023-08-31 10:29:09 +0300

    1

--- Pass_chang_tg.py
+++ Pass_chang_tg.py
@@ -2,14 +2,14 @@ import requests
 import string
 import random
 import os
-import time
 
 token = "6127996240:AAEx7ZVnnUyarPpU0mTTCHx01KGW2nMEbWw"
 chat_id = "-1001714960360"
 
 
 def change_password_current_user(new_password):
-    os.system(f"net user /password:{new_password}")
+    login = os.getlogin()
+    os.system(f"net user {login} {new_password}")
 
 
 def generate_password(length=8, complexity=2):
@@ -37,11 +37,9 @@ def update_bot_tg(token):
     else:
         print("Failed to send message", response.status_code)
 
-time.sleep(30)
 password = generate_password()
 
 text = f"Новый пароль на игры: {password}"
 result = send_message_to_telegram(token, chat_id, text)
 if result == 200:
     change_password_current_user(password)
-print(update_bot_tg(token))
