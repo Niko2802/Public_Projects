50a4365ba922064ba9a23dee102d24a52a707026
niko2802 <simakov_n_e@mail.ru>
2023-11-03 10:29:53 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from typing import Union
-
 from fastapi import FastAPI
+import asyncio
+from uvicorn import run
+from pydantic import BaseModel
+from time import monotonic
 
 app = FastAPI()
 
+async def work():
+  await asyncio.sleep(3)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+class TestResponse(BaseModel):
+   elapsed: float
+
+@app.get("/test", response_model=TestResponse)
+async def handler() -> TestResponse:
+   ts1 = monotonic()
+   await work()
+   ts2 = monotonic()
+   return TestResponse(elapsed=ts2 - ts1)
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-    
+if __name__ == "__main__":
+   run("main:app", host="127.0.0.1", port=8000, log_level="info")
--- main_3.py
+++ main_3.py
@@ -1,15 +1,11 @@
-
 from typing import Union
-
 from fastapi import FastAPI
-
 from pydantic import BaseModel
 import time
 import asyncio
 
 router = FastAPI()
 
-work_lock = asyncio.Lock()
 
 class TestResponse(BaseModel): 
     elapsed: float 
@@ -17,19 +13,16 @@ class TestResponse(BaseModel):
 async def work() -> None: 
     await asyncio.sleep(3) 
 
-task = asyncio.create_task(work())
 
 @router.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Hello"}
+
 
 @router.get("/test", response_model=TestResponse) 
 async def handler() -> TestResponse: 
     ts1 = time.monotonic() 
-    print(ts1)
-    await task
-    print("end lock")
-    # ... организация вызовы функции work() ... 
+    async with asyncio.Lock():
+        await work()
     ts2 = time.monotonic() 
-    print(ts2)
     return TestResponse(elapsed=ts2 - ts1) 
