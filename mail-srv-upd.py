4961ac23b547a1016704444ec63568fa4e5878ca
Niko2802 <simakov_n_e@mail.ru>
2023-12-15 12:32:53 +0300

    1

--- mail-srv-upd.py
+++ mail-srv-upd.py
@@ -15,18 +15,18 @@ subject_label = tk.Label(window, text="Тема письма:")
 subject_label.grid(row=0, column=0)
 
 subject = tk.Text(window, height=3, width=50)
-subject.grid(row=1, column=0)
+subject.grid(row=1, column=0, rowspan=3, columnspan=5)
 
 cb_nn_val = tk.BooleanVar()
 cb_nn = tk.Checkbutton(window, text="нн.", variable=cb_nn_val)
-cb_nn.grid(row=2, column=0)
+cb_nn.grid(row=4, column=0)
 
 cb_uk_val = tk.BooleanVar()
 cb_uk = tk.Checkbutton(window, text="ук.", variable=cb_uk_val)
-cb_uk.grid(row=2, column=1)
+cb_uk.grid(row=4, column=1)
 
 button_send = tk.Button(window, text="Отправить", command=on_button_send_click)
-button_send.grid(row=3, column=0)
+button_send.grid(row=6, column=0)
 
 
 
