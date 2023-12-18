0c4194021f57fb8625106f3ae7d325f8596c26e1
Niko2802 <simakov_n_e@mail.ru>
2023-12-18 08:53:59 +0300

    1

--- mail-srv-upd.py
+++ mail-srv-upd.py
@@ -2,8 +2,21 @@ import tkinter as tk
 
 factory = ["агтф", "уз", "тн-юг", "в", "м", "мф", "нн", "тн-нн", "ну", "ук", "уч"]
 
+class Ch_box():
+    def __init__(self, window, name, row, col):
+        self.name = name
+        self.value = tk.BooleanVar()
+        self.ch_box = tk.Checkbutton(window, text=name, variable=self.value)
+        self.ch_box.grid(row=row, column=col, sticky="w")
+
+    def get_value(self):
+        return self.value.get()
+
+
 def on_button_send_click():
-    pass
+    for i in list_ch_boxes:
+        print(i.get_value())
+
 
 window = tk.Tk()
 window.title("Заявка в ИТ")
@@ -15,12 +28,9 @@ subject_label.grid(row=0, column=0)
 subject = tk.Text(window, height=3, width=50)
 subject.grid(row=1, column=0, rowspan=3, columnspan=5)
 
-
+list_ch_boxes = []
 for i in factory:
-    cb_val = tk.BooleanVar()
-    cb = tk.Checkbutton(window, text=i, variable=cb_val)
-    cb.grid(row=4 + factory.index(i), column=0, sticky="w")
-
+    list_ch_boxes.append(Ch_box(window, i, 4 + factory.index(i), 0))
 
 button_send = tk.Button(window, text="Отправить", command=on_button_send_click)
 button_send.grid(row=15, column=0)
