202641919d0cb33c6660985cdf445e37833916ba
Nikolay Simakov <simakov.nikolay@gmail.com>
2024-11-06 12:44:31 +0300

    1

--- test-2.py
+++ test-2.py
@@ -1,13 +1,6 @@
-def declension_minutes(num):
-    if num % 10 == 1 and num % 100 != 11: 
-        return "минута"
-    elif 2 <= num % 10 <= 4 and (num % 100 < 10 or num % 100 >= 20):
-        return "минуты"
-    else:
-        return "минут"
-    
-print(declension_minutes(3))
-print(declension_minutes(11))
-print(declension_minutes(21))
-print(declension_minutes(13))
-print(declension_minutes(14))
+import win32net
+
+users = win32net.NetUserEnum(None, 0, 0, 0)
+
+for user in users[0]:
+    print(user['name'])
